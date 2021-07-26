9590651dbefc04caffebf33f842d97e511da80a8
vishalpwaman <63911951+vishalpwaman@users.noreply.github.com>
2021-07-26 19:18:08 +0530

    Added: Add Insert Operation Using Xml File database connection.

---
+++ CRUDXmlDbProject/Helper/SqlQueries.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDXmlDbProject.Helper
+{
+    public class SqlQueries
+    {
+
+        static IConfiguration queriesConfig = new ConfigurationBuilder()
+            .AddXmlFile("SqlQueries.xml", true, true)
+            .Build();
+
+        public static string InsertEmployeeData { get { return queriesConfig["InsertEmployeeData"]; } }
+
+    }
+}
